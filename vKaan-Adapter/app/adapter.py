718a0d10a23fef9a54e3a2aff8bf1da5da8a16b1
vmbro <onuryuzseven@gmail.com>
2024-10-18 16:51:13 +0300

    initial-release

---
+++ vKaan-Adapter/app/adapter.py
@@ -0,0 +1,202 @@
+#  Copyright 2024 vKaan MP, vmbro.com
+#  Author: Onur Yuzseven
+import atexit
+import json
+import sys
+import aria.ops.adapter_logging as logging
+import constants
+from typing import Any
+from typing import List
+from typing import Optional
+from aria.ops.adapter_instance import AdapterInstance
+from aria.ops.definition.adapter_definition import AdapterDefinition
+from aria.ops.object import Object
+from aria.ops.result import CollectResult
+from aria.ops.result import EndpointResult
+from aria.ops.result import TestResult
+from aria.ops.suite_api_client import key_to_object
+from aria.ops.suite_api_client import SuiteApiClient
+from aria.ops.definition.units import Units
+from aria.ops.timer import Timer
+from constants import ADAPTER_KIND
+from constants import ADAPTER_NAME
+from pyVim.connect import Disconnect
+from pyVim.connect import SmartConnect
+from cluster import add_cluster_metrics
+
+logger = logging.getLogger(__name__)
+
+
+def get_adapter_definition() -> AdapterDefinition:
+    with Timer(logger, "Get Adapter Definition"):
+        definition = AdapterDefinition(ADAPTER_KIND, ADAPTER_NAME)
+
+        definition.define_string_parameter(
+            constants.HOST_IDENTIFIER,
+            "vCenter Server",
+            description="FQDN or IP address of the vCenter Server instance.",
+        )
+        definition.define_int_parameter(
+            constants.PORT_IDENTIFIER, "Port", default=443, advanced=True
+        )
+
+        credential = definition.define_credential_type("vsphere_user", "Credential")
+        credential.define_string_parameter(constants.USER_CREDENTIAL, "User Name")
+        credential.define_password_parameter(constants.PASSWORD_CREDENTIAL, "Password")
+
+        CCR = definition.define_object_type("Cluster Compute Resource")
+        clusterConfiguration = CCR.define_group("Cluster Configuration")
+
+        vSphereHA = clusterConfiguration.define_group("vSphere HA")
+        vSphereHA.define_string_property("Host Monitoring")
+        vSphereHA.define_string_property("Response \\\\ Host Isolation")
+        vSphereHA.define_string_property("Response \\\\ Default VM Restart Priority")
+        vSphereHA.define_string_property("Response \\\\ Datastore APD")
+        vSphereHA.define_string_property("Response \\\\ Datastore PDL")
+        vSphereHA.define_string_property("VM Monitoring")    
+        vSphereHA.define_string_property("Heartbeat Datastore")
+
+        DRS = clusterConfiguration.define_group("DRS Configuration")
+        DRS.define_string_property("Proactive DRS")
+        DRS.define_string_property("Scale Descendants Shares")
+        DRS.define_metric("DRS Score", unit=Units.RATIO.PERCENT)
+
+        logger.debug(f"Returning adapter definition: {definition.to_json()}")
+
+    return definition
+
+
+def test(adapter_instance: AdapterInstance) -> TestResult:
+    with Timer(logger, "Test connection"):
+        result = TestResult()
+        try:
+            logger.debug(f"Returning test result: {result.get_json()}")
+
+            service_instance = _get_service_instance(adapter_instance)
+            content = service_instance.RetrieveContent()
+            logger.info(f"content: {content}")
+
+        except Exception as e:
+            logger.error("Unexpected connection test error")
+            logger.exception(e)
+            result.with_error("Unexpected connection test error: " + repr(e))
+        finally:
+            return result
+
+
+def collect(adapter_instance: AdapterInstance) -> CollectResult:
+    with Timer(logger, "Collection"):
+        result = CollectResult()
+        try:
+            logger.debug(f"Returning collection result {result.get_json()}")
+            service_instance = _get_service_instance(adapter_instance)
+            content = service_instance.RetrieveContent()
+            logger.error(f"taskManager: {content.taskManager}")
+
+            with adapter_instance.suite_api_client as client:
+                adapter_instance_id = _get_vcenter_adapter_instance_id(
+                    client, adapter_instance
+                )
+                if adapter_instance_id is None:
+                    result.with_error(
+                        f"No vCenter Adapter Instance found matching vCenter Server '{adapter_instance.get_identifier_value(constants.HOST_IDENTIFIER)}'"
+                    )
+                    return result
+                add_cluster_metrics(client, adapter_instance_id, result, content)
+
+        except Exception as e:
+            logger.error("Unexpected collection error")
+            logger.exception(e)
+            result.with_error("Unexpected collection error: " + repr(e))
+        finally:
+            return result
+
+
+def get_endpoints(adapter_instance: AdapterInstance) -> EndpointResult:
+    with Timer(logger, "Get Endpoints"):
+        result = EndpointResult()
+        logger.debug(f"Returning endpoints: {result.get_json()}")
+        return result
+
+
+def _get_service_instance(
+    adapter_instance: AdapterInstance,
+) -> Any:
+    host = adapter_instance.get_identifier_value(constants.HOST_IDENTIFIER)
+    port = int(adapter_instance.get_identifier_value(constants.PORT_IDENTIFIER, 443))
+    user = adapter_instance.get_credential_value(constants.USER_CREDENTIAL)
+    password = adapter_instance.get_credential_value(constants.PASSWORD_CREDENTIAL)
+
+    service_instance = SmartConnect(
+        host=host, port=port, user=user, pwd=password, disableSslCertValidation=True
+    )
+
+    atexit.register(Disconnect, service_instance)
+
+    return service_instance
+
+
+def _get_vcenter_adapter_instance_id(
+    client: SuiteApiClient, adapter_instance: Object
+) -> Optional[str]:
+    ais: List[Object] = client.query_for_resources(
+        {
+            "adapterKind": [constants.VCENTER_ADAPTER_KIND],
+            "resourceKind": ["VMwareAdapter Instance"],
+        }
+    )
+    vcenter_server = adapter_instance.get_identifier_value(constants.HOST_IDENTIFIER)
+    for ai in ais:
+        logger.debug(
+            f"Considering vCenter Adapter Instance with VCURL: {ai.get_identifier_value('VCURL')}"
+        )
+        if ai.get_identifier_value("VCURL") == vcenter_server:
+            return _get_adapter_instance_id(client, ai)
+    return None
+
+
+def _get_adapter_instance_id(
+    client: SuiteApiClient, adapter_instance: Object
+) -> Optional[Any]:
+    response = client.get(
+        f"api/adapters?adapterKindKey={adapter_instance.get_key().adapter_kind}"
+    )
+    if response.status_code < 300:
+        for ai in json.loads(response.content).get("adapterInstancesInfoDto", []):
+            adapter_instance_key = key_to_object(ai.get("resourceKey")).get_key()
+            if adapter_instance_key == adapter_instance.get_key():
+                return ai.get("id")
+    return None
+
+
+def main(argv: List[str]) -> None:
+    logging.setup_logging("adapter.log")
+    logging.rotate()
+    logger.info(f"Running adapter code with arguments: {argv}")
+    if len(argv) != 3:
+        logger.error("Arguments must be <method> <inputfile> <ouputfile>")
+        exit(1)
+
+    method = argv[0]
+
+    if method == "test":
+        test(AdapterInstance.from_input()).send_results()
+    elif method == "endpoint_urls":
+        get_endpoints(AdapterInstance.from_input()).send_results()
+    elif method == "collect":
+        collect(AdapterInstance.from_input()).send_results()
+    elif method == "adapter_definition":
+        result = get_adapter_definition()
+        if type(result) is AdapterDefinition:
+            result.send_results()
+        else:
+            logger.info(
+                "get_adapter_definition method did not return an AdapterDefinition"
+            )
+            exit(1)
+    else:
+        logger.error(f"Command {method} not found")
+        exit(1)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ vKaan-Adapter/app/cluster.py
@@ -0,0 +1,62 @@
+#  Copyright 2024 vKaan MP, vmbro.com
+#  Author: Onur Yuzseven
+import logging
+from typing import Any
+from typing import List
+from aria.ops.object import Object
+from aria.ops.result import CollectResult
+from aria.ops.suite_api_client import SuiteApiClient
+from constants import VCENTER_ADAPTER_KIND
+from pyVmomi import vim
+
+logger = logging.getLogger(__name__)
+
+def add_cluster_metrics(
+    suite_api_client: SuiteApiClient,
+    adapter_instance_id: str,
+    result: CollectResult,
+    content: Any,
+) -> None:
+    container = content.rootFolder
+    view_type = [vim.ClusterComputeResource]
+    recursive = True
+    container_view = content.viewManager.CreateContainerView(
+        container, view_type, recursive
+    )
+
+    clusters: List[Object] = suite_api_client.query_for_resources(
+        {
+            "adapterKind": [VCENTER_ADAPTER_KIND],
+            "resourceKind": ["ClusterComputeResource"],
+            "adapterInstanceId": [adapter_instance_id],
+        }
+    )
+
+    clusters_by_uuid: dict[str, Object] = {
+        cluster.get_identifier_value("VMEntityObjectID"): cluster for cluster in clusters
+    }
+
+    children = container_view.view
+    for cluster in children:
+        cluster_obj = clusters_by_uuid.get(cluster._moId)
+        if cluster_obj:
+            if cluster.configuration.dasConfig.hostMonitoring == 'enabled':
+                hostMonitoring = True
+            else:
+                hostMonitoring = False
+
+            cluster_obj.with_property("configuration|dasConfig|Host Monitoring", bool(hostMonitoring))
+            cluster_obj.with_property("configuration|dasConfig|Response \ Host Isolation", str(cluster.configuration.dasConfig.defaultVmSettings.isolationResponse))
+            cluster_obj.with_property("configuration|dasConfig|Response \ Default VM Restart Priority", str(cluster.configuration.dasConfig.defaultVmSettings.restartPriority))
+            cluster_obj.with_property("configuration|dasConfig|Response \ Datastore APD", str(cluster.configuration.dasConfig.defaultVmSettings.vmComponentProtectionSettings.vmStorageProtectionForAPD))
+            cluster_obj.with_property("configuration|dasConfig|Response \ Datastore PDL", str(cluster.configuration.dasConfig.defaultVmSettings.vmComponentProtectionSettings.vmStorageProtectionForPDL))
+            cluster_obj.with_property("configuration|dasConfig|VM Monitoring", str(cluster.configuration.dasConfig.vmMonitoring))         
+            cluster_obj.with_property("configuration|dasConfig|Heartbeat Datastore", str(cluster.configuration.dasConfig.hBDatastoreCandidatePolicy))
+            cluster_obj.with_property("configuration|drsConfig|Proactive DRS", str(cluster.configurationEx.proactiveDrsConfig.enabled))
+            cluster_obj.with_property("configuration|drsConfig|Scale Descendants Shares", str(cluster.configuration.drsConfig.scaleDescendantsShares))
+            cluster_obj.with_metric("configuration|drsConfig|DRS Score (%)", int(cluster.summary.drsScore))
+            result.add_object(cluster_obj)
+        else:
+            logger.warning(
+                f"Could not find Cluster '{cluster.name}' with MoID: {cluster._moId}."
+            )
